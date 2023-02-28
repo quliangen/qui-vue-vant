b5c770efaa6de4c69e946acc626ee2664b0388bf
quliangen <quliangen@outlook.com>
2023-02-28 22:24:02 +0800

    deploy

---
+++ assets/quickstart.c795042c.js
@@ -0,0 +1,9 @@
+import{o as a,a as s,z as t}from"./vue-libs.8e33bf5d.js";const e={class:"van-doc-markdown-body"},c=t(`<h1>\u5FEB\u901F\u4E0A\u624B</h1><div class="van-doc-card"><h3 id="an-zhuang" tabindex="-1">\u5B89\u88C5</h3><pre><code class="language-bash"><span class="hljs-comment"># \u901A\u8FC7 npm</span>
+npm i @infra/qui-vue-vant
+
+<span class="hljs-comment"># \u901A\u8FC7 yarn</span>
+yarn add @infra/qui-vue-vant
+
+<span class="hljs-comment"># \u901A\u8FC7 pnpm</span>
+pnpm add @infra/qui-vue-vant
+</code></pre></div>`,2),o=[c],l={__name:"quickstart",setup(r,{expose:n}){return n({frontmatter:{}}),(p,i)=>(a(),s("div",e,o))}};export{l as default};
